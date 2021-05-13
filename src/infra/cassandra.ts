7af773cd0fbf9ddb9cc5d8786f30d71e50c170d6
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-05-13 20:29:37 -0300

    feat: connect to cassandra and execute simple queries

--- src/infra/cassandra.ts
+++ src/infra/cassandra.ts
@@ -2,5 +2,5 @@ import { Client } from 'cassandra-driver';
 
 export const client = new Client({
   contactPoints: ['127.0.0.1'],
-  localDataCenter: 'dc1',
+  localDataCenter: 'datacenter1',
 });
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,55 @@
+import { types as CassandraTypes } from 'cassandra-driver';
 import { client } from '@infra/cassandra';
+
+const id = CassandraTypes.Uuid.random();
 
 const main = async () => {
   await client.connect();
 
-  console.log(`Keyspaces: ${client.metadata.keyspaces}`);
+  await client.execute(`
+    CREATE KEYSPACE IF NOT EXISTS examples 
+    WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3' }
+  `);
+
+  await client.execute(`
+    CREATE TABLE IF NOT EXISTS examples.basic (
+      id uuid, 
+      txt text, 
+      val int, 
+      PRIMARY KEY(id)
+    )
+  `);
+
+  await client.execute(
+    `
+    INSERT INTO examples.basic (id, txt, val)
+    VALUES (?, ?, ?)
+  `,
+    [id, 'Hello!', 100],
+    { prepare: true }
+  );
+
+  const result = await client.execute(
+    `
+    SELECT id, txt, val 
+      FROM examples.basic 
+     WHERE id = ?
+  `,
+    [id],
+    { prepare: true }
+  );
+
+  const rawData = result.rows;
+
+  rawData.forEach((data) => {
+    console.log({ id: data.id.toString(), txt: data.txt, val: data.val });
+  });
 
   await client.shutdown();
 };
 
-main().catch((err) => console.log('Unexpected Error', err));
+main().catch((err) => {
+  console.log('Unexpected Error', err.message);
+
+  process.exit(1);
+});
